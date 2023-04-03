eba56cf5c3f3f6bc4b7594c1f964b51ba2c871f0
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-04-03 14:32:50 -0500

    autosave script

--- src/routes/+layout.sever.ts
+++
@@ -1,8 +0,0 @@
-import { getServerSession } from "@supabase/auth-helpers-sveltekit";
-import type { LayoutServerLoad } from "./$types";
-
-// This function is called at build time, and is used to generate the layout
-export const load: LayoutServerLoad = async (event) => {
-    return {
-        session: await getServerSession(event),
-};
